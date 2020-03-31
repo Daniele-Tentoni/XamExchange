d5f4c2a5f6127641a36d5712885fa0995883a6f9
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-03-31 14:23:39 +0200

    Xamarin Forms project creation.

---
+++ XamExchange/XamExchange/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamExchange.Models;
+
+namespace XamExchange.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
