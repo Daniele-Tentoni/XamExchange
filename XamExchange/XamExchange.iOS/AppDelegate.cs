d015450cbe2225c3ae3a27001a8870b354e17408
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-04-09 11:34:09 +0200

    Add Visual Material styles.

--- XamExchange/XamExchange.iOS/AppDelegate.cs
+++ XamExchange/XamExchange.iOS/AppDelegate.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿using Foundation;
 using UIKit;
 
 namespace XamExchange.iOS
@@ -22,7 +18,8 @@ namespace XamExchange.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
+            Xamarin.Forms.Forms.Init();
+            Xamarin.Forms.FormsMaterial.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
