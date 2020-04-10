5b043347cf69840e98be645f7c668fc5e3456b97
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-04-10 17:59:14 +0200

    Spostamento della pagina di about.

---
+++ XamExchange/XamExchange/Views/About/AboutTabPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamExchange.Views.About
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutTabPage : TabbedPage
+    {
+        public AboutTabPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
